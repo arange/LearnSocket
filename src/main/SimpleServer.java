74e1bf25ada19d2fb6257d689b24ded558c0b954
arange <panpan2558@gmail.com>
2018-08-01 15:56:45 +1000

    added loop for server to listen

--- src/main/SimpleServer.java
+++ src/main/SimpleServer.java
@@ -7,15 +7,19 @@ public class SimpleServer {
 public static void main(String args[]) throws IOException {
 //Register service on port 1254
 ServerSocket s = new ServerSocket(1234);
-Socket s1=s.accept(); // Wait and accept a connection
-//Get a communication stream associated with the socket
-OutputStream s1out = s1.getOutputStream();
-DataOutputStream dos = new DataOutputStream (s1out);
-//Send a string!
-dos.writeUTF("Hi there");
-//Close the connection, but not the server socket
-dos.close();
-s1out.close();
-s1.close();
+int count = 1;
+while(true) {
+	Socket s1=s.accept(); // Wait and accept a connection
+	//Get a communication stream associated with the socket
+	OutputStream s1out = s1.getOutputStream();
+	DataOutputStream dos = new DataOutputStream (s1out);
+	//Send a string!
+	count++;
+	dos.writeUTF("Hi there, this is connection " + count);
+	//Close the connection, but not the server socket
+	dos.close();
+	s1out.close();
+	s1.close();
+}
 }
 }
