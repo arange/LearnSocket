5041f4f06b8ec8a6bb4a1cca04bb983736b84db3
arange <panpan2558@gmail.com>
2018-08-01 15:37:45 +1000

    copy the code from chapter 13

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,21 @@
 package main;
 
+//SimpleServer.java: A simple server program.
+import java.net.*;
+import java.io.*;
 public class Main {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.printf("hello wolrd");
-	}
-
+public static void main(String args[]) throws IOException {
+//Register service on port 1254
+ServerSocket s = new ServerSocket(1254);
+Socket s1=s.accept(); // Wait and accept a connection
+//Get a communication stream associated with the socket
+OutputStream s1out = s1.getOutputStream();
+DataOutputStream dos = new DataOutputStream (s1out);
+//Send a string!
+dos.writeUTF("Hi there");
+//Close the connection, but not the server socket
+dos.close();
+s1out.close();
+s1.close();
+}
 }
