f098c6e7ad3215a4ea1c1450996a6f7f91f143c7
arange <panpan2558@gmail.com>
2018-08-03 01:01:27 +1000

    Command+Shif+F auto formatting" "

--- src/main/SimpleClient.java
+++ src/main/SimpleClient.java
@@ -1,19 +1,21 @@
 package main;
+
 // SimpleClient.java: A simple client program.
 import java.net.*;
 import java.io.*;
+
 public class SimpleClient {
-public static void main(String args[]) throws IOException {
+	public static void main(String args[]) throws IOException {
 // Open your connection to a server, at port 1234
-Socket s1 = new Socket("localhost",1234);
+		Socket s1 = new Socket("localhost", 1234);
 // Get an input file handle from the socket and read the input
-InputStream s1In = s1.getInputStream();
-DataInputStream dis = new DataInputStream(s1In);
-String st = new String (dis.readUTF());
-System.out.println(st);
+		InputStream s1In = s1.getInputStream();
+		DataInputStream dis = new DataInputStream(s1In);
+		String st = new String(dis.readUTF());
+		System.out.println(st);
 // When done, just close the connection and exit
-dis.close();
-s1In.close();
-s1.close();
-}
+		dis.close();
+		s1In.close();
+		s1.close();
+	}
 }
--- src/main/UDPServer.java
+++ src/main/UDPServer.java
@@ -3,36 +3,32 @@ package main;
 // UDPServer.java: A simple UDP server program.
 import java.net.*;
 import java.io.*;
+
 public class UDPServer {
-	public static void main(String args[]){
+	public static void main(String args[]) {
 		DatagramSocket aSocket = null;
 		if (args.length < 1) {
-		System.out.println("Usage: java UDPServer <Port Number>");
-		System.exit(1);
+			System.out.println("Usage: java UDPServer <Port Number>");
+			System.exit(1);
 		}
 		try {
-		int socket_no = Integer.valueOf(args[0]).intValue();
-		aSocket = new DatagramSocket(socket_no);
-		byte[] buffer = new byte[1000];
-		while(true) {
-		DatagramPacket request = new DatagramPacket(buffer,
-		buffer.length);
-		aSocket.receive(request);
-		DatagramPacket reply = new DatagramPacket(request.getData(),
-		request.getLength(),request.getAddress(),
-		request.getPort());
-		aSocket.send(reply);
+			int socket_no = Integer.valueOf(args[0]).intValue();
+			aSocket = new DatagramSocket(socket_no);
+			byte[] buffer = new byte[1000];
+			while (true) {
+				DatagramPacket request = new DatagramPacket(buffer, buffer.length);
+				aSocket.receive(request);
+				DatagramPacket reply = new DatagramPacket(request.getData(), request.getLength(), request.getAddress(),
+						request.getPort());
+				aSocket.send(reply);
+			}
+		} catch (SocketException e) {
+			System.out.println("Socket: " + e.getMessage());
+		} catch (IOException e) {
+			System.out.println("IO: " + e.getMessage());
+		} finally {
+			if (aSocket != null)
+				aSocket.close();
 		}
-		}
-		catch (SocketException e) {
-		System.out.println("Socket: " + e.getMessage());
-		}
-		catch (IOException e) {
-		System.out.println("IO: " + e.getMessage());
-		}
-		finally {
-		if (aSocket != null)
-		aSocket.close();
-		}
-		}
-		}
+	}
+}
